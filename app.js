a1e2888bf08eac2822d8d8a3b62e996366cac565
Hari Prasath <Khariprasath351@gmail.com>
2023-01-09 17:38:29 +0530

    todo application is completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let bodyParser = require('body-parser');
 let app = express();
 
 const itemList = ['Bootcamp', 'Digital Marketing']
+const workList =['work','work']
+let listTitle=''
 
 app.use(bodyParser.urlencoded({ extended:"true"}));
 
@@ -10,13 +12,33 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index', {list: itemList});
+
+      listTitle ='Home'
+    res.render('index', {
+        titlek:listTitle,
+        list: itemList
+    });
 });
 
+app.get('/work',(req,res)=>{
+     listTitle = 'work';
+    res.render('index',{
+        titlek:listTitle,
+        list: workList
+    })
+})
+
 app.post('/',(req,res)=>{
-    const item = req.body.newTask;
-    itemList.push(item);
-    res.redirect('/')
+    if(req.body.btn === 'Home'){
+        const item = req.body.newTask;
+        itemList.push(item);
+        res.redirect('/')
+    }else{
+        const item = req.body.newTask;
+        workList.push(item);
+        res.redirect('/work')
+    }
+    
 })
 
 app.listen(3000,()=>{
